23a8bf6ab0aadf9ade0643e3692d68a05ae57ab2
alexDemche <alexeyydemchenko@gmail.com>
2019-10-07 17:38:44 +0300

    Updates

---
+++ precache-manifest.2ece8939550b99f973bda881dfa3acff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31fe21f822350a7a0f78d40be9df08c9",
+    "url": "/react_redux-list-of-todos/index.html"
+  },
+  {
+    "revision": "0e4197f40e80e28342a4",
+    "url": "/react_redux-list-of-todos/static/css/main.0b00124d.chunk.css"
+  },
+  {
+    "revision": "cfc18c2d1cfbdcc71f5c",
+    "url": "/react_redux-list-of-todos/static/js/2.91022780.chunk.js"
+  },
+  {
+    "revision": "0e4197f40e80e28342a4",
+    "url": "/react_redux-list-of-todos/static/js/main.0ee84862.chunk.js"
+  },
+  {
+    "revision": "37dc44999646af065e8e",
+    "url": "/react_redux-list-of-todos/static/js/runtime~main.054cc81e.js"
+  }
+]);
